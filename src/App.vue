50dab473a17ba9d3b1747d19976a043903248ca8
Flawid DSouza <me@flawiddsouza.com>
2022-06-16 16:29:02 +0530

    feat: make app a PWA

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
     <Frame />
+    <ReloadPrompt />
 </template>
 
 <script setup lang="ts">
 import Frame from './components/Frame.vue'
+import ReloadPrompt from './components/ReloadPrompt.vue'
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,44 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()]
+    plugins: [
+        vue(),
+        VitePWA({
+            includeAssets: [
+                'favicon.png',
+                'favicon.ico',
+                'robots.txt',
+                'apple-touch-icon.png',
+                'fonts/source-sans-pro-v21-latin-regular.woff2',
+                'fonts/source-sans-pro-v21-latin-regular.woff'
+            ],
+            manifest: {
+                name: 'Socketfox',
+                short_name: 'Socketfox',
+                description: 'Allows you to connect and send data to different websocket URLs and manage those connections efficiently',
+                theme_color: '#ffffff',
+                icons: [
+                    {
+                        src: 'pwa-192x192.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    },
+                    {
+                        src: 'pwa-512x512.png',
+                        sizes: '512x512',
+                        type: 'image/png',
+                        purpose: 'any maskable'
+                    }
+                ]
+            }
+        })
+    ]
 })
