0934fc7ce9eb1db07aecfb58032aed8bb0fc5396
Flawid DSouza <me@flawiddsouza.com>
2022-09-23 13:17:33 +0530

    feat: display seconds in message timestamp

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,7 +2,7 @@ import { nanoid } from 'nanoid'
 import dayjs from 'dayjs'
 
 export function formatTimestamp(epoch: number) {
-    return dayjs(epoch).format('YYYY-MM-DD hh:mm A')
+    return dayjs(epoch).format('YYYY-MM-DD hh:mm:ss A')
 }
 
 export function generateId() {
